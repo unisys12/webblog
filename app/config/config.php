c5fd5e339ff381b3fe0e3ddc5ae66f748998c791
unisys12 <unisys12@gmail.com>
2014-03-02 11:52:09 -0600

    remove vendor autoloader from config

--- app/config/config.php
+++ app/config/config.php
@@ -15,7 +15,7 @@ return new \Phalcon\Config(array(
         'pluginsDir'     => __DIR__ . '/../../app/plugins/',
         'libraryDir'     => __DIR__ . '/../../app/library/',
         'cacheDir'       => __DIR__ . '/../../app/cache/',
-        'vendorDir'      => __DIR__ . '/../../vendor/autoload.php',
+        //'vendorDir'      => __DIR__ . '/../../vendor/autoload.php',
         'baseUri'        => '/blog-local/',
     )
 ));
