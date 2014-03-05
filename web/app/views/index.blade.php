3fb0048ae799b385ebee2b9751774f9d90288e4f
unisys12 <unisys12@gmail.com>
2014-03-05 16:53:56 -0600

    update post var usage

--- web/app/views/index.blade.php
+++ web/app/views/index.blade.php
@@ -10,11 +10,11 @@ Blog
 	@foreach ($posts as $post)
 	<header class="post_header">
 		<h3 class="post_title"><a href="">{{ $post->post_title }}</a></h3>
-		<span class="post_details"><small>{{ $post.post_author }} - {{ $post.created_at }}</small></span>
-		<h5 class="post_desc">{{ $post.post_desc }}</h5>
+		<span class="post_details"><small>{{ $post->post_author }} - {{ $post->created_at }}</small></span>
+		<h5 class="post_desc">{{ $post->post_desc }}</h5>
 	</header>
 	<article class="post_content">
-		{{$$post.post_content}}
+		{{$$post->post_content}}
 	</article>
 	@endforeach
 </main>
