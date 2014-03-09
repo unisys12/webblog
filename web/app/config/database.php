52214519c38d74896dd6863c39a75d575b50c960
unisys12 <unisys12@gmail.com>
2014-03-09 14:56:06 -0500

    comment out local db connection

--- web/app/config/database.php
+++ web/app/config/database.php
@@ -63,7 +63,7 @@ return array(
 			'prefix'    => '',
 		),
 
-		'mysql' => array(
+		/*'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
 			'database'  => 'blog_local',
@@ -72,7 +72,7 @@ return array(
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
-		),
+		),*/
 
 		'pgsql' => array(
 			'driver'   => 'pgsql',
