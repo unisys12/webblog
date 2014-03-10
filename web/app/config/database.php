8a2a370830c6fa9ed7f2188abc16ddef825e5b38
unisys12 <unisys12@gmail.com>
2014-03-09 20:26:50 -0500

    again with the db config

--- web/app/config/database.php
+++ web/app/config/database.php
@@ -52,7 +52,7 @@ return array(
 			'prefix'   => '',
 		),
 
-		/*'mysql' => array(
+		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.7.111.130',
 			'database'  => 'blog_local',
@@ -61,9 +61,9 @@ return array(
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
-		),*/
+		),
 
-		'mysql' => array(
+		/*'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
 			'database'  => 'blog_local',
@@ -72,7 +72,7 @@ return array(
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
-		),
+		),*/
 
 		'pgsql' => array(
 			'driver'   => 'pgsql',
