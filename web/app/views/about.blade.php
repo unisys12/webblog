0e25f5e906661101dc26ef08cdac03fa54567ee3
unisys12 <unisys12@gmail.com>
2014-03-12 18:01:09 -0500

    updated small nav with background color change on click

--- web/app/views/about.blade.php
+++ web/app/views/about.blade.php
@@ -6,8 +6,12 @@ Nothing Interesting
 
 @section('content')
 
-<main class="posts">
-	<h1>Seriously! Nothing to see here people! Yet anyway... check back later. Now shew!</h1>
+<main class="page_content">
+	<h2>Interests</h2>
+	<p>I have been fascinated with electronics from a very young age. Always wanting to take things apart to see how they worked. Growing up in a fairly bad part of Mississippi, school was not my friend. So I never applied myself. Always looking forward to the day I could graduated and get away. College or University was never in my future. Half-way through high school, I enrolled in a Auto Mechanics course and did pretty well. It was the first time that I actually felt like I could do something with myself. One day, our teacher was out sick and no sub could be found, so they split our class up and placed us in several different other classes offered by the vocational school. I just so happened to be placed in what was called, "Diversified Technologies" for a week. In this class, students were exposed to light computer programming on old Macs (appleII's actually), but all did other things like write programs that controlled robotic arms and all sorts of cool stuff like that. Basically the equivalent modern day <a href="http://en.wikipedia.org/wiki/STEM_fields">STEM</a> classes some middle and high school students can take that introduce them to certain paths of study. This was were I realized what I hugh mistake I had made in regards to my future. It was quite depressing. I ended up as a pretty good, but unhappy mechanic for roughly 10 yrs.</p>
+	<h2>Day Job</h2>
+	<p>My day job, for the last 15 yrs or so, is working as a copier technician. I first worked at Xerox for 6 or 7 yrs and currently at a Savin dealership for the last 7, almost 8 yrs. It's an awesome job and I love the work. This career has allowed me to pursue the things that I truly love and have been interested in since I was very young. And, for the most part, have excelled at it.</p>
+	<p>After about 4 yrs at Xerox, digital copiers were really starting to take off. So we all had to go to school on computers. Why? Well, todays copiers are just tall computers with paper trays and an attached scanner. This is the only formal training I have where computers are concerned. Everything else was either self taught or looked up on the Internet.</p>
 </main>
 
 @stop
