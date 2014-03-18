544a45c1f292d56d2ca64e8ff7e11e06a1393e52
unisys12 <unisys12@gmail.com>
2014-03-18 07:36:04 -0500

    removed content parsing from model

--- web/app/models/Post.php
+++ web/app/models/Post.php
@@ -4,19 +4,5 @@ class Post extends Eloquent{
 
 	protected $guarded = array('id');
 
-	public function parsePostContent()
-	{
-		$posts = Post::all();
-
-       // Loop through the posts results;
-       // Grab only the post contents and parse it with Parsedown;
-       // When passing content to View, use substr to only display
-       // 500 characters of content;
-       foreach ($posts as $post) {
-           $content = Parsedown::instance()->parse($post->post_content);
-       }
-
-       return $content;
-	}
 
 }
