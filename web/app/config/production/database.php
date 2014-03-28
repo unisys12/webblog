13921647925095a347520887f0fe090e2af00a34
unisys12 <unisys12@gmail.com>
2014-03-27 22:20:04 -0500

    update production db values

--- web/app/config/production/database.php
+++ web/app/config/production/database.php
@@ -6,10 +6,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => "$_ENV['OPENSHIFT_MYSQL_DB_HOST']",
+			'host'      => $_ENV['OPENSHIFT_DIY_MYSQL_DB_HOST'],
 			'database'  => 'blog_local',
-			'username'  => "$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']",
-			'password'  => "$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']",
+			'username'  => $_ENV['OPENSHIFT_DIY_MYSQL_DB_USERNAME'],
+			'password'  => $_ENV['OPENSHIFT_DIY_MYSQL_DB_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
