a5949ac0c67a301d83911f2e5c19e00ef61e2438
Phillip Jackson <unisys12@gmail.com>
2014-05-07 03:11:45 +0000

    update production settings

--- web/bootstrap/start.php
+++ web/bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(function()
 {
-	return getenv('LARAVEL_ENV') ? : '';
+	return getenv('LARAVEL_ENV') ? : 'production';
 });
 
 /*
